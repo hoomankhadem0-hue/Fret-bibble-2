a6e16c7ef2f4710631e9f38f1bd4910f9a5a8045
hoomankhadem0-hue <hooman.khadem0@gmail.com>
2026-09-19 23:44:41 +0330

    Fix rememberSaveable import

--- app/src/main/java/com/whoman/fretbible/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/whoman/fretbible/ui/screens/HomeScreen.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.whoman.fretbible.ui.components.AnimatedBackground
