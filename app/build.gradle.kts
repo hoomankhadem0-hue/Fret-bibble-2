dc571771d00e9ca0c637d1fdfa0d4dfdb9ac7736
hoomankhadem0-hue <hooman.khadem0@gmail.com>
2026-09-19 23:18:43 +0330

    Use Activity Compose version compatible with Android 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2025.10.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
-    implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
