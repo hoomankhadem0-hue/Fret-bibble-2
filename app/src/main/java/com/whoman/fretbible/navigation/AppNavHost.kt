4f9b14f8d93a85f50d7411dbf6a642b98ff8b2cf
hoomankhadem0-hue <hooman.khadem0@gmail.com>
2026-09-21 13:14:36 +0330

    Add navigation bar elevation import

--- app/src/main/java/com/whoman/fretbible/navigation/AppNavHost.kt
+++ app/src/main/java/com/whoman/fretbible/navigation/AppNavHost.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.padding
 import com.whoman.fretbible.ui.screens.*
 import com.whoman.fretbible.ui.theme.*
