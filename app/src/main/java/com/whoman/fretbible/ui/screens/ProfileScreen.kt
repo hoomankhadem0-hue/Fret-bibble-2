42bbdaf9dc06d30525747947c03efb773a2daf72
hoomankhadem0-hue <hooman.khadem0@gmail.com>
2026-09-21 18:18:41 +0330

    Fix profile animation import

--- app/src/main/java/com/whoman/fretbible/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/whoman/fretbible/ui/screens/ProfileScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 import com.whoman.fretbible.R
 import com.whoman.fretbible.audio.AudioSettings
