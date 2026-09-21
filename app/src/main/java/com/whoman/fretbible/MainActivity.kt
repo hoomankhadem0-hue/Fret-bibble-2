008c1135ce4774a094373e95e66cd9d1fccb7b11
hoomankhadem0-hue <hooman.khadem0@gmail.com>
2026-09-21 13:14:25 +0330

    Show animated splash screen on app launch

--- app/src/main/java/com/whoman/fretbible/MainActivity.kt
+++ app/src/main/java/com/whoman/fretbible/MainActivity.kt
@@ -3,7 +3,9 @@ package com.whoman.fretbible
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
 import com.whoman.fretbible.navigation.AppNavHost
+import com.whoman.fretbible.ui.screens.SplashScreen
 import com.whoman.fretbible.ui.theme.FretBibleTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +13,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FretBibleTheme {
-                AppNavHost()
+                var splash by remember { mutableStateOf(true) }
+                if (splash) {
+                    SplashScreen(onFinished = { splash = false })
+                } else {
+                    AppNavHost()
+                }
             }
         }
     }
