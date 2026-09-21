4b8d5a50a9ad1b9f9293e227269ea779fae136f6
hoomankhadem0-hue <hooman.khadem0@gmail.com>
2026-09-21 13:16:55 +0330

    Fix practice animation import

--- app/src/main/java/com/whoman/fretbible/ui/screens/PracticeScreen.kt
+++ app/src/main/java/com/whoman/fretbible/ui/screens/PracticeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
